1ac7a6cf5a2d2ca7c426c540671d722bddd786e6
Dax Wann <daxwann@gmail.com>
2019-05-04 23:44:52 -0700

    add ArtworkShare model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,16 @@ class Artwork < ApplicationRecord
   validates :artist_id, :image_url, presence: true
   validates :title, presence: true, uniqueness: { scope: :artist_id }
 
+  belongs_to :artist,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: :User
+
+  has_many :artwork_shares,
+    primary_key: :id,
+    foreign_key: :artwork_id
+
+  has_many :shared_viewers,
+    through: :artwork_shares,
+    source: :viewer
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,41 @@
 
 ActiveRecord::Base.transaction do
   User.destroy_all
+  Artwork.destroy_all
+  ArtworkShare.destroy_all
   
-  u1 = User.create!(username: 'Marcel')
-  u2 = User.create!(username: 'Jasper')
-  u3 = User.create!(username: 'Leonardo')
-  u4 = User.create!(username: 'Ruth')
-  u5 = User.create!(username: 'Claude')
+  User.create!(username: 'Marcel')
+  User.create!(username: 'Jasper')
+  User.create!(username: 'Leonardo')
+  User.create!(username: 'Ruth')
+  User.create!(username: 'Claude')
+
+  Artwork.create!(title: "Urinal", image_url: "www.urinal.com", artist_id: 1)
+  Artwork.create!(title: "Wheel", image_url: "www.marcel.com/wheel", artist_id: 1)
+  Artwork.create!(title: "Target", image_url: "www.target.com", artist_id: 2)
+  Artwork.create!(title: "American Flag", image_url: "www.jasper.com/flag", artist_id: 2)
+  Artwork.create!(title: "Last Supper", image_url: "www.supper.com", artist_id: 3)
+  Artwork.create!(title: "Mona Lisa", image_url: "www.davinci.com/monalisa", artist_id: 3)
+  Artwork.create!(title: "Untitled", image_url: "www.ruth.com/untitled", artist_id: 4)
+  Artwork.create!(title: "Sculpture", image_url: "www.ruth.com/sculpture", artist_id: 4)
+  Artwork.create!(title: "Untitled", image_url: "www.claude.com/untitled", artist_id: 5)
+  Artwork.create!(title: "Self-portrait", image_url: "www.claude.com/selfportrait", artist_id: 5)
+
+  ArtworkShare.create!(artwork_id: 1, viewer_id: 2)
+  ArtworkShare.create!(artwork_id: 1, viewer_id: 4)
+  ArtworkShare.create!(artwork_id: 2, viewer_id: 3)
+  ArtworkShare.create!(artwork_id: 2, viewer_id: 5)
+  ArtworkShare.create!(artwork_id: 3, viewer_id: 1)
+  ArtworkShare.create!(artwork_id: 3, viewer_id: 4)
+  ArtworkShare.create!(artwork_id: 4, viewer_id: 4)
+  ArtworkShare.create!(artwork_id: 5, viewer_id: 2)
+  ArtworkShare.create!(artwork_id: 6, viewer_id: 5)
+  ArtworkShare.create!(artwork_id: 6, viewer_id: 1)
+  ArtworkShare.create!(artwork_id: 7, viewer_id: 1)
+  ArtworkShare.create!(artwork_id: 7, viewer_id: 3)
+  ArtworkShare.create!(artwork_id: 8, viewer_id: 2)
+  ArtworkShare.create!(artwork_id: 9, viewer_id: 3)
+  ArtworkShare.create!(artwork_id: 9, viewer_id: 4)
+  ArtworkShare.create!(artwork_id: 10, viewer_id: 4)
+  ArtworkShare.create!(artwork_id: 10, viewer_id: 1)
 end
